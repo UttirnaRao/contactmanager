70232c1c0a5848e494efbe229e55792b0ec3645d
Uttirna Rao <uttirnarao4@gmail.com>
2020-05-17 09:03:53 +0530

    Updates

---
+++ precache-manifest.f61fe50f4c8ab04c8146327aa725c694.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9011df3d55424dc2ccb03e72b5af0b7a",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "a0dcf84777fefb97712f",
+    "url": "/contactmanager/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "72b71b6afc3a2127a555",
+    "url": "/contactmanager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "a0dcf84777fefb97712f",
+    "url": "/contactmanager/static/js/2.d5894894.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/contactmanager/static/js/2.d5894894.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72b71b6afc3a2127a555",
+    "url": "/contactmanager/static/js/main.f5e47874.chunk.js"
+  },
+  {
+    "revision": "99d97f0521117940c2d2",
+    "url": "/contactmanager/static/js/runtime-main.4f607e48.js"
+  }
+]);
